28f62bc9aaec2bff49c5c01dc65b8bd7f1ef23f7
maxscherbakov <max-tsh@yandex.ru>
2023-10-04 11:09:49 +0300

    new comand

--- BMP.h
+++ BMP.h
@@ -55,10 +55,11 @@ struct BMP {
     void gauss(int r) {
         std::vector <uint8_t> data2((bmp_info_header.width + r *2 ) * (bmp_info_header.height + r * 2) * bmp_info_header.bit_count / 8);
         uint32_t channles = bmp_info_header.bit_count / 8; 
+        int padding_now = (4 - channles * bmp_info_header.width % 4) % 4;
         for (uint y = 0; y < bmp_info_header.height; y++){
             for (uint x = 0; x < bmp_info_header.width; x++){
-                for (int i = 0; i < channles; i++) {
-                data2[channles * (x+r + (y+r)*(bmp_info_header.width+r*2)) + i] = data[channles * (x + y * bmp_info_header.width) + i];
+                for (int pix = 0; pix < channles; pix++) {
+                data2[channles * (x+r + (y+r)*(bmp_info_header.width+r*2)) + pix + padding_now * (y+r)] = data[channles * (x + y * bmp_info_header.width) + pix + padding_now*y];
                 }
             }
         }
@@ -71,73 +72,24 @@ struct BMP {
                 for (int i = -r; i < r; i++){
                     for (int j = -r; j < r; j++){
                         for (int pix = 0; pix < channles; pix++) {
-                            d[pix] += (1/ pow(e, (i*i + j*j)/(2*r*r))) * data2[channles * ((x+r + i) + (bmp_info_header.width + r * 2) * (y + r + j)) + pix];
+                            d[pix] += (1.2/ pow(e, (i*i + j*j)/(2*r*r))) * data2[channles * ((x+r + i) + (bmp_info_header.width + r * 2) * (y + r + j)) + pix + (y + r + j)*padding_now];
                         }
                     }
                 }
                 for (int pix = 0; pix < channles; pix++) {
-                    data[channles * (x + y * bmp_info_header.width)+pix] = d[pix] * (1/(2 * PI * r * r));
+                    data[channles * (x + y * bmp_info_header.width)+pix+y*padding_now] = d[pix] * (1/(2 * PI * r * r));
 
                 }
             }
         }
     }
     
-    void gauss_run(int r) {
-        std::vector <uint8_t> data2((bmp_info_header.width + r *2 ) * (bmp_info_header.height + r * 2) * bmp_info_header.bit_count / 8);
-        uint32_t channles = bmp_info_header.bit_count / 8; 
-        for (uint y = 0; y < bmp_info_header.height; y++){
-            for (uint x = 0; x < bmp_info_header.width; x++){
-                for (int i = 0; i < channles; i++) {
-                data2[channles * (x+r + (y+r)*(bmp_info_header.width+r*2)) + i] = data[channles * (x + y * bmp_info_header.width) + i];
-                }
-            }
-        }
-        const double PI = 3.141592653589793;
-        double e = exp(1);
-        std:: vector <double> d(4);
-        for (uint y = 0; y < bmp_info_header.height; y++){
-            for (uint x = 0; x < bmp_info_header.width; x++){
-                d = {0, 0, 0, 0};
-                for (int i = -r; i < r; i++){
-                    for (int pix = 0; pix < channles; pix++) {
-                        d[pix] += (1/ pow(e, (i*i)/(2*r*r))) * data2[channles * ((x+r + i) + (bmp_info_header.width + r * 2) * (y + r)) + pix];
-                    }
-                }
-                for (int pix = 0; pix < channles; pix++) {
-                    data[channles * (x + y * bmp_info_header.width)+pix] = d[pix] * (1/(2 * PI * r * r));
-                }
-            }
-        }
-        for (uint y = 0; y < bmp_info_header.height; y++){
-            for (uint x = 0; x < bmp_info_header.width; x++){
-                for (int i = 0; i < channles; i++) {
-                data2[channles * (x+r + (y+r)*(bmp_info_header.width+r*2)) + i] = data[channles * (x + y * bmp_info_header.width) + i];
-                }
-            }
-        }
-        for (uint y = 0; y < bmp_info_header.height; y++){
-            for (uint x = 0; x < bmp_info_header.width; x++){
-                d = {0, 0, 0, 0};
-                for (int j = -r; j < r; j++){
-                    if (y + j >= 0 && y + j < bmp_info_header.height) {
-                        for (int pix = 0; pix < channles; pix++) {
-                            d[pix] += (1/ pow(e, (j*j)/(2*r*r))) * data2[channles * ((x+r) + (bmp_info_header.width+2*r) * (y + r + j)) + pix];
-                        }
-
-                    }
-                }
-                for (int pix = 0; pix < channles; pix++) {
-                    data[channles * (x + y * bmp_info_header.width)+pix] = d[pix];
-                }
-            }
-        }
-    }
 
     void read(const char *fname) {
     	std:: ifstream inp{fname, std::ios_base::binary};
         if (inp) {
             inp.read((char*)&file_header, sizeof(file_header));
+            if(file_header.file_type != 0x4D42) throw std::runtime_error("Error! Unrecognized file format.");
             inp.read((char*)&bmp_info_header, sizeof(bmp_info_header));
 
             if (bmp_info_header.bit_count == 32) {
@@ -149,6 +101,8 @@ struct BMP {
             data_rab.resize(file_header.offset_data - sizeof(file_header) - sizeof(bmp_info_header));
             inp.read((char*)data_rab.data(), data_rab.size());
             inp.read((char*)data.data(), data.size());
+        } else {
+            throw std::runtime_error("Unable to open the input image file.");
         }
         inp.close();
     }
@@ -156,45 +110,35 @@ struct BMP {
     void verr_right(){
         std::vector <uint8_t> data2(data);
         uint32_t channels = bmp_info_header.bit_count / 8;
+        int padding_now = (4 - channels * bmp_info_header.width % 4) % 4;
+        int padding_next = (4 - channels * bmp_info_header.height % 4) % 4;
 
         for (uint32_t y = 0; y < bmp_info_header.height; y++){
             for (uint32_t x = 0; x < bmp_info_header.width; x++){
-                int32_t ind = channels * ((bmp_info_header.width - x - 1) * bmp_info_header.height + y);
-                data[ind] = data2[channels * (x + y *  bmp_info_header.width)];
-                if (channels > 1) {
-                    data[ind + 1] = data2[channels * (x + y *  bmp_info_header.width) + 1];
-                    data[ind + 2] = data2[channels * (x + y *  bmp_info_header.width) + 2];
-                    if (channels == 4) {
-                        data[ind + 3] = data2[channels * (x + y *  bmp_info_header.width) + 3];
-                    }
+                int32_t ind = channels * ((bmp_info_header.width - x - 1) * bmp_info_header.height + y) + (bmp_info_header.width - x - 1) * padding_next;
+                for (int pix = 0; pix < channels; pix++) {
+                    data[ind+pix] = data2[channels * (x + y * bmp_info_header.width)+pix + y*padding_now];
                 }
             }
         }
-        bmp_info_header.height += bmp_info_header.width;
-        bmp_info_header.width = bmp_info_header.height - bmp_info_header.width;
-        bmp_info_header.height -= bmp_info_header.width;
+        std:: swap(bmp_info_header.width, bmp_info_header.height);
     }
 
     void verr_left() {
         std::vector <uint8_t> data2(data);
         uint32_t channels = bmp_info_header.bit_count / 8;
+        int padding_now = (4 - channels * bmp_info_header.width % 4) % 4;
+        int padding_next = (4 - channels * bmp_info_header.height % 4) % 4;
 
         for (uint32_t y = 0; y < bmp_info_header.height; y++){
             for (uint32_t x = 0; x < bmp_info_header.width; x++){
-                int32_t ind = channels * ((bmp_info_header.height - y - 1) + x * bmp_info_header.height);
-                data[ind] = data2[channels * (x + y *  bmp_info_header.width)];
-                if (channels > 1) {
-                    data[ind + 1] = data2[channels * (x + y *  bmp_info_header.width) + 1];
-                    data[ind + 2] = data2[channels * (x + y *  bmp_info_header.width) + 2];
-                    if (channels == 4) {
-                        data[ind + 3] = data2[channels * (x + y *  bmp_info_header.width) + 3];
-                    }
+                int32_t ind = channels * ((bmp_info_header.height - y - 1) + x * bmp_info_header.height) + x * padding_next;
+                for (int pix = 0; pix < channels; pix++) {
+                    data[ind+pix] = data2[channels * (x + y *  bmp_info_header.width)+pix+padding_now*y];
                 }
             }
         }
-        bmp_info_header.height += bmp_info_header.width;
-        bmp_info_header.width = bmp_info_header.height - bmp_info_header.width;
-        bmp_info_header.height -= bmp_info_header.width;
+        std:: swap(bmp_info_header.width, bmp_info_header.height);
     }
 
     void write(const char *fname) {
--- main.cpp
+++ main.cpp
@@ -4,14 +4,24 @@
 int main() {
 
     BMP bmp("mind_8.bmp");
-    std:: cout << bmp.file_header.file_size;
+    std:: cout << bmp.file_header.file_size << "\n";
     bmp.write("out.bmp");
     bmp.verr_right();
     bmp.write("out_right.bmp");
     bmp.verr_left();
     bmp.write("out_right_left.bmp");
-    bmp.gauss_run(1);
-    bmp.write("out_gauss.bmp");
+    bmp.gauss(5);
+    bmp.write("out_gauss_2.bmp");
+
+    BMP bmp2("geom_24.bmp");
+    std:: cout << bmp2.file_header.file_size << "\n";
+    bmp2.write("out.bmp");
+    bmp2.verr_right();
+    bmp2.write("out_right.bmp");
+    bmp2.verr_left();
+    bmp2.write("out_right_left.bmp");
+    bmp2.gauss(5);
+    bmp2.write("out_gauss.bmp");
 
 
 
