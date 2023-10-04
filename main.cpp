526295b4101f51aec9958ae3c63a4a07c4c68ecd
maxscherbakov <max-tsh@yandex.ru>
2023-10-04 11:14:25 +0300

    final version 1.0

--- main.cpp
+++ main.cpp
@@ -5,23 +5,23 @@ int main() {
 
     BMP bmp("mind_8.bmp");
     std:: cout << bmp.file_header.file_size << "\n";
-    bmp.write("out.bmp");
+    bmp.write("mind_8_out.bmp");
     bmp.verr_right();
-    bmp.write("out_right.bmp");
+    bmp.write("mind_8_right.bmp");
     bmp.verr_left();
-    bmp.write("out_right_left.bmp");
+    bmp.write("mind_8_right_left.bmp");
     bmp.gauss(5);
-    bmp.write("out_gauss_2.bmp");
+    bmp.write("mind_8_gauss.bmp");
 
     BMP bmp2("geom_24.bmp");
-    std:: cout << bmp2.file_header.file_size << "\n";
-    bmp2.write("out.bmp");
+    std:: cout << "size geom_24: " << bmp2.file_header.file_size << "\n";
+    bmp2.write("geom_24_out.bmp");
     bmp2.verr_right();
-    bmp2.write("out_right.bmp");
+    bmp2.write("geom_24_right.bmp");
     bmp2.verr_left();
-    bmp2.write("out_right_left.bmp");
+    bmp2.write("geom_24_right_left.bmp");
     bmp2.gauss(5);
-    bmp2.write("out_gauss.bmp");
+    bmp2.write("geom_24_gauss.bmp");
 
 
 
