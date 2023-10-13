538172d52d532d2fe1e23dc6c445630b3ae88832
maxscherbakov <max-tsh@yandex.ru>
2023-10-13 14:51:35 +0300

    version 3.4

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 int main() {
     // 8 бит файл с шириной кратной 4
     BMP bmp("mind_8.bmp");
-    std:: cout << "size mind_8.bmp:" << bmp.file_header.file_size << "\n";
     bmp.write("mind_8_out.bmp");
     bmp.turn_right();
     bmp.write("mind_8_right.bmp");
@@ -16,7 +15,6 @@ int main() {
 
     //24 бит файл с шириной не кратной 4
     BMP bmp2("geom_24.bmp");
-    std:: cout << "size geom_24.bmp:" << bmp2.file_header.file_size << "\n";
     bmp2.write("geom_24_out.bmp");
     bmp2.turn_right();
     bmp2.write("geom_24_right.bmp");
@@ -27,7 +25,6 @@ int main() {
     bmp2.write("geom_24_gauss.bmp");
 
    BMP bmp3("test.bmp");
-    std:: cout << "size test.bmp:" << bmp3.file_header.file_size << "\n";
     bmp3.write("test_out.bmp");
     bmp3.turn_right();
     bmp3.write("test_right.bmp");
